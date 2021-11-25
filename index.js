0355b14da795eef30c1137d7afe305a627c7a54a
binaryleo <leomouracss@fastmail.com>
2021-11-25 00:28:16 -0300

    css update - canvas random art

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const btnEl = document.getElementById("btnUser");
+const starEl = document.getElementById("stars");
+const followersEl = document.getElementById("followers");
+const gravatarEl = document.getElementById("gravatar");
 function run() {
   let stars = 0;
   userEl = document.getElementById("gituser").value;
@@ -10,12 +13,39 @@ function run() {
       for (const repo of data) {
         stars += repo.stargazers_count;
       }
-      console.log("Stars earned: ", stars);
+      starEl.innerHTML =`Stars earned: ${stars}`;
     });
   fetch(api)
     .then((data) => data.json())
     .then((data) => {
-      console.log("Number of followers: ", data.followers);
+      let followers = data.followers;
+      let gravatar = data.avatar_url;
+      followersEl.innerHTML = `Followers: ${followers}`;
+      gravatarEl.src = gravatar
     });
 }
 btnEl.addEventListener("click", run);
+setInterval(loadCanvasEffect,1000);
+function loadCanvasEffect(){
+  let canvas = document.querySelector("canvas");
+  let context = canvas.getContext("2d");
+  const width = 40;
+  const height = 40;
+  const gap = 5;
+  context.strokeStyle="#00fdff";
+  let x, y;
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      context.beginPath();
+      x = 20 + (width + gap) * i;
+      y = 20 + (width + gap) * j;
+      context.rect(x, y, width, height);
+      context.stroke();
+      if (Math.random() > 0.5) {
+        context.beginPath();
+        context.rect(x + 8, y + 8, width - 16, height - 16);
+        context.stroke();
+      }
+    }
+  }
+}
